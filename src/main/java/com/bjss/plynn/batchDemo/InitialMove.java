190723f5b980d1c00513b08170edbd754d5fa511
Peter A. Lynn <peter.lynn@bjss.com>
2016-01-28 14:06:23 -0500

    A checkpoint

--- src/main/java/com/bjss/plynn/batchDemo/InitialMove.java
+++ src/main/java/com/bjss/plynn/batchDemo/InitialMove.java
@@ -8,7 +8,6 @@ import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 
-import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Date;
@@ -22,16 +21,10 @@ public class InitialMove implements Tasklet {
 
     Logger myLogger = LoggerFactory.getLogger(InitialMove.class);
 
-    /* Getter and setter for injected value */
-    public String getfileNameInjected() {
-        return fileNameInjected;
-    }
-
-    public void setfileNameInjected(String fileNameInjected) {
-        this.fileNameInjected = fileNameInjected;
-    }
-
+    /* Injected value */
     private String fileNameInjected = "";
+    public String getfileNameInjected() { return fileNameInjected; }
+    public void setfileNameInjected(String fileNameInjected) { this.fileNameInjected = fileNameInjected; }
 
     /*
     Two methods of passing parameters to the batch are illustrated.  The variable "fileNameInjected" is injected
@@ -41,41 +34,52 @@ public class InitialMove implements Tasklet {
      */
     public RepeatStatus execute( StepContribution arg0, ChunkContext arg1 ) throws Exception {
 
-        String directoryName = (String) arg1.getStepContext().getJobParameters().get("inDirectoryName");
+        PathFinder pathFinder;
+        {
         /* Check the job execution context for "changedFileName" to see if a previous step had
         to change the file name to avoid a collision. */
-        String fileName =  arg1.getStepContext().getStepExecution().getJobExecution().getExecutionContext().
-                getString("changedFileName", fileNameInjected);
-        myLogger.info ("\n***************************************\nThe input directory is {}.  The arriving file is {}\n***************************************",
-                directoryName, fileName);
+            String fileName = arg1.getStepContext().getStepExecution().getJobExecution().getExecutionContext().
+                    getString("changedFileName", fileNameInjected);
+            String directoryName = (String) arg1.getStepContext().getJobParameters().get("inDirectoryName");
+            pathFinder = PathFinder.getPathFinder(directoryName, fileName);
+        }
+
          // Get Path objects to source file and working location
-        Path inFilePath = FileSystems.getDefault().getPath(directoryName, fileName);
+        Path inFilePath = pathFinder.getInputFilePath();
+        Path workFilePath = pathFinder.getWorkFilePath();
+        Path doneFilePath = pathFinder.getDoneFilePath();
+
+        myLogger.info ("\n***************************************\n");
         myLogger.info("The path object is {}, and the path fileName is {}", inFilePath, inFilePath.getFileName());
-        Path workFilePath=FileSystems.getDefault().getPath(directoryName, "/inFlight", fileName);
         myLogger.info("The work path object is {}, and the work path fileName is {}", workFilePath, workFilePath.getFileName());
         arg0.setExitStatus(ExitStatus.COMPLETED);
+
+        if (Files.exists(doneFilePath) || Files.exists(workFilePath))
+        {
+           long now = new Date().getTime();
+            String append = "." + Long.toHexString(now);
+            if (Files.exists(workFilePath)) {
+                myLogger.info("{} already exists.  Add timestamp {}",
+                        workFilePath, append);
+            } else {
+                myLogger.info("{} already exists.  Add timestamp {}",
+                        doneFilePath, append);
+            }
+            pathFinder.setFileName(pathFinder.getFileName().concat(append));
+            workFilePath = pathFinder.getWorkFilePath();
+            doneFilePath = pathFinder.getDoneFilePath();
+            arg1.getStepContext().getStepExecution().getJobExecution().getExecutionContext()
+                    .put("changedFileName", pathFinder.getFileName());
+        }
         try {
             // Move the file
             Files.move(inFilePath, workFilePath);
             myLogger.info("Moved file {} to {}", inFilePath, workFilePath);
-        } catch (java.nio.file.FileAlreadyExistsException exists) {
-            // If a work file with the same directoryName exists, add an arbitrary sequence.
-            // Take the current date/time in hexadecimal.
-            long now = new Date().getTime();
-            String append = "." + Long.toHexString(now);
-            String newFileName = fileName.concat(append);
-            workFilePath = FileSystems.getDefault().getPath(directoryName, "/inFlight", newFileName);
-            myLogger.info("{} already exists.  Add timestamp and move file to {} instead.", inFilePath, workFilePath);
-            Files.move(inFilePath, workFilePath);
-            /* ... and put that modified file name into the job execution context */
-            arg1.getStepContext().getStepExecution().getJobExecution().getExecutionContext()
-                    .put("changedFileName", newFileName);
-            myLogger.info("Moved file {} to {}", inFilePath, workFilePath);
-        } catch (Exception e) {
+         } catch (Exception e) {
             // and fail if that doesn't work either.
             myLogger.error("Got an error in moving {} to {}.", inFilePath, workFilePath);
             myLogger.error(e.toString());
-         arg0.setExitStatus(ExitStatus.FAILED);
+            arg0.setExitStatus(ExitStatus.FAILED);
         }
         return RepeatStatus.FINISHED;
     }
--- src/main/java/com/bjss/plynn/batchDemo/ProcessTaskletCode.java
+++ src/main/java/com/bjss/plynn/batchDemo/ProcessTaskletCode.java
@@ -22,48 +22,62 @@ public class ProcessTaskletCode implements Tasklet {
 
     Logger myLogger = LoggerFactory.getLogger(ProcessTaskletCode.class);
 
-    /* Getter and setter for injected value */
+    /* Injected value */
+    private String fileNameInjected = "";
     public String getfileNameInjected() {
         return fileNameInjected;
     }
-
     public void setfileNameInjected(String fileNameInjected) {
         this.fileNameInjected = fileNameInjected;
     }
 
-    private String fileNameInjected = "";
 
     /*
     Two methods of passing parameters to the batch are illustrated.  The variable "fileNameInjected" is injected
     by the batch definition demoOne.xml, from the run parameter outFileName.  The directory name is
     taken from the parameters directly through the step context, from inFileName.
-      Note also the use of the job execution context to pass changed file names to later steps.
+      Note also the use of the job execution context to get changed file names from earlier steps.
      */
     public RepeatStatus execute( StepContribution arg0, ChunkContext arg1 ) throws Exception {
 
-        String directoryName = (String) arg1.getStepContext().getJobParameters().get("inDirectoryName");
+        PathFinder pathFinder;
+        {
         /* Check the job execution context for "changedFileName" to see if a previous step had
         to change the file name to avoid a collision. */
-        String fileName =  arg1.getStepContext().getStepExecution().getJobExecution().getExecutionContext().
-                getString("changedFileName", fileNameInjected);
-        myLogger.info ("\n***************************************\nThe processing directory is {} plus inFlight.  The processing file is {}\n***************************************",
-                directoryName, fileName);
-         // Get Path object to the file to process
-        Path workFilePath=FileSystems.getDefault().getPath(directoryName, "/inFlight", fileName);
+            String fileName = arg1.getStepContext().getStepExecution().getJobExecution().getExecutionContext().
+                    getString("changedFileName", fileNameInjected);
+            String directoryName = (String) arg1.getStepContext().getJobParameters().get("inDirectoryName");
+            pathFinder = PathFinder.getPathFinder(directoryName, fileName);
+        }
+        // Get Path object to the file to process
+        Path workFilePath=pathFinder.getWorkFilePath();
+        Path doneFilePath=pathFinder.getWorkFilePath();
+        myLogger.info ("\n***************************************");
         myLogger.info("The work path object is {}, and the work path fileName is {}", workFilePath, workFilePath.getFileName());
         arg0.setExitStatus(ExitStatus.COMPLETED);
 
-
         try {
-            // Move the file
+            // "Process" the file
             long howbig = Files.size(workFilePath);
             myLogger.info("The size of the file is {} bytes", howbig);
         } catch (Exception e) {
             // and fail if that doesn't work either.
-            myLogger.error("Got an error in prodessing {}.", workFilePath);
+            myLogger.error("Got an error in processing {}.", workFilePath);
             myLogger.error(e.toString());
-         arg0.setExitStatus(ExitStatus.FAILED);
+            arg0.setExitStatus(ExitStatus.FAILED);
+            return RepeatStatus.FINISHED;
         }
+        try {
+            // Move the file
+            Files.move(workFilePath, doneFilePath);
+            myLogger.info("Moved processed file {} to {}", workFilePath, doneFilePath);
+        } catch (Exception e) {
+            // and fail if that doesn't work either.
+            myLogger.error("Got an error in moving {} to {}.", workFilePath, doneFilePath);
+            myLogger.error(e.toString());
+            arg0.setExitStatus(ExitStatus.FAILED);
+        }
+
         return RepeatStatus.FINISHED;
     }
 }
